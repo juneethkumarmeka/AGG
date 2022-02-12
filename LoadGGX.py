e20338330d0c3bb46335f0e43bfe0427088487e3
Juneeth Kumar Meka <mekajr@mail.uc.edu>
2022-02-12 13:56:51 -0500

    Load GGX file

---
+++ LoadGGX.py
@@ -0,0 +1,34 @@
+"""
+Created on Sat Feb 12 13:43:53 2022
+
+@author: JuneethKumar Meka
+
+Project Name : Graph Grammar Attribute Benchmark Generator
+"""
+
+#Modules
+#-----------------------------------------------------------------------------#
+import os
+#-----------------------------------------------------------------------------#
+
+#Source Code
+#-----------------------------------------------------------------------------#
+class LoadGGX:
+    def __init__(self,file):
+        if os.path.isfile(file):
+            self.file = file 
+        else:
+            raise Exception("{} not found".format(file))
+    
+    def __call__(self):
+        os.system('java -Dfile.encoding=UTF-8 \
+                  -classpath "AGG/bin" \
+                  agg_execution.Importing {}'.format(self.file))
+        
+#-----------------------------------------------------------------------------#
+
+#Testing
+#-----------------------------------------------------------------------------#
+# LoadGGX("Linear_Structure.ggx")()
+#-----------------------------------------------------------------------------#
+
