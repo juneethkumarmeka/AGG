64d48559d135a028d7d7c77522a52a89c7b42690
amarkant23394 <amarkant23394@gmail.com>
2022-02-20 20:38:32 -0500

    Added code to set node attributes

--- Graph_Analyzer/testing.py
+++ Graph_Analyzer/testing.py
@@ -1,9 +1,12 @@
 import Detector
 import analyser
 
-for node in range(7):
-    fanin = 2 + node
-    fanout = 9 - fanin
-    print("fan in = ",fanin, "fanout = ",fanout)
-    #obj = Detector.Detector(9,fanin,fanout,"benchmark_verilog_code","sub_graph_directory")
-obj = analyser.Analyser("JSON_9_2_7.json")
+for i in range(3,11):
+    nodes = i
+    for node in range(nodes-2):
+        fanin = 2 + node
+        fanout = nodes - fanin
+        print("fan in = ",fanin, "fanout = ",fanout)
+        obj = Detector.Detector(nodes,fanin,fanout,"benchmark_verilog_code","sub_graph_directory")
+
+#obj = analyser.Analyser("JSON_9_2_7.json")
